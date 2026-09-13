8a2c78709ac3454fb77a10a84fe636b9d9d5d3a0
GMB8281 <287333904+GMB8281@users.noreply.github.com>
2026-09-13 15:12:40 -0300

    Revert "2.11092026 => critical bug fixes"
    This reverts commit 1c882dd58cb8c8a28776434bfa823720a2b6b9d0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 1
-        versionName = "2.11092026"
+        versionName = "2.10092026"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
